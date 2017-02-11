2a7586d02fdf153b545085f2138b45083d6b0d27
taurins <taurins94@gmail.com>
2017-02-11 22:59:31 +0200

    Update

--- CTCO_task/Form1.cs
+++ CTCO_task/Form1.cs
@@ -12,9 +12,31 @@ namespace CTCO_task
 {
     public partial class Form1 : Form
     {
+        public class record
+        {
+            public string name { get; set; }
+            public string service { get; set; }
+            public decimal money { get; set; }
+        }
+
+        //List<record> data = new List<record>();
+        BindingList<record> data = new BindingList<record>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            record rec = new record();
+            rec.name = textBox1.Text;
+            rec.service = textBox2.Text;
+            rec.money = numericUpDown1.Value;
+            data.Add(rec);
+            BindingSource source = new BindingSource(data, null);
+            dataGridView1.DataSource = source;
+
         }
     }
 }
