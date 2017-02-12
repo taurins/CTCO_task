54d3d85c9d4df487ba4c8d9d8b64f551fdfeaf3f
taurins <taurins94@gmail.com>
2017-02-12 17:01:10 +0200

    Update

--- CTCO_task/Form1.Designer.cs
+++ CTCO_task/Form1.Designer.cs
@@ -36,15 +36,24 @@
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(82, 44);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
             this.textBox1.TabIndex = 0;
             // 
             // label1
@@ -69,8 +78,8 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(82, 83);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(128, 20);
-            this.textBox2.TabIndex = 2;
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
+            this.textBox2.TabIndex = 1;
             // 
             // label3
             // 
@@ -86,15 +95,15 @@
             this.numericUpDown1.DecimalPlaces = 2;
             this.numericUpDown1.Location = new System.Drawing.Point(82, 123);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(128, 20);
-            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Size = new System.Drawing.Size(176, 20);
+            this.numericUpDown1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(308, 44);
+            this.button1.Location = new System.Drawing.Point(183, 160);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
+            this.button1.TabIndex = 3;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -102,19 +111,93 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 169);
+            this.dataGridView1.Location = new System.Drawing.Point(312, 80);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(382, 243);
+            this.dataGridView1.Size = new System.Drawing.Size(342, 180);
             this.dataGridView1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total: 0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Average: 0";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(33, 323);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(225, 192);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(309, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Data";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Expenses";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(312, 324);
+            this.dataGridView3.MultiSelect = false;
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView3.Size = new System.Drawing.Size(342, 192);
+            this.dataGridView3.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(309, 292);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Transactions";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 441);
+            this.ClientSize = new System.Drawing.Size(692, 546);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
@@ -127,6 +210,8 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +227,13 @@
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- CTCO_task/Form1.cs
+++ CTCO_task/Form1.cs
@@ -12,31 +12,56 @@ namespace CTCO_task
 {
     public partial class Form1 : Form
     {
-        public class record
-        {
-            public string name { get; set; }
-            public string service { get; set; }
-            public decimal money { get; set; }
-        }
+        
+        DataTable table = new DataTable();
+        DataTable table1 = new DataTable();
+        bool changes;
 
-        //List<record> data = new List<record>();
-        BindingList<record> data = new BindingList<record>();
+        decimal total=0;
 
         public Form1()
         {
             InitializeComponent();
+            table.Columns.Add("name", typeof(string));
+            table.Columns.Add("service", typeof(string));
+            table.Columns.Add("money", typeof(int));
+            dataGridView1.DataSource = table;
+
+            table1.Columns.Add("name", typeof(string));
+            table1.Columns.Add("money", typeof(int));
+            dataGridView2.DataSource = table1;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            record rec = new record();
-            rec.name = textBox1.Text;
-            rec.service = textBox2.Text;
-            rec.money = numericUpDown1.Value;
-            data.Add(rec);
-            BindingSource source = new BindingSource(data, null);
-            dataGridView1.DataSource = source;
+            total += numericUpDown1.Value;
 
+            table.Rows.Add(textBox1.Text, textBox2.Text, numericUpDown1.Value);                     
+
+            label4.Text = "Toatl: "+Convert.ToString(total);
+
+            
+
+            if (table1.Rows.Count==0)
+                table1.Rows.Add(textBox1.Text, numericUpDown1.Value);
+            else
+            {
+                changes = true;
+                foreach (DataRow row in table1.Rows)
+                {
+                    if (row["name"].ToString() == textBox1.Text)
+                    {
+                        row["money"] = Convert.ToString(Convert.ToDecimal(row["money"].ToString()) + numericUpDown1.Value);
+                        changes = false;
+                        break;
+                    }
+                }
+                if(changes)
+                table1.Rows.Add(textBox1.Text, numericUpDown1.Value);
+            }
+
+            label5.Text = "Average: " +( total/table1.Rows.Count);
+            
         }
     }
 }
